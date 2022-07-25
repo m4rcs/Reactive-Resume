19fc8fc370ccc0b226c81548388bca11d9f09e3b
Marc Schiller <marc.schiller@inpixon.com>
2022-07-25 10:58:03 +0200

    Center Image

--- client/templates/Gengar/widgets/Masthead.tsx
+++ client/templates/Gengar/widgets/Masthead.tsx
@@ -25,50 +25,53 @@ export const MastheadSidebar: React.FC = () => {
   const iconColor = useMemo(() => (contrast === 'dark' ? theme.text : theme.background), [theme, contrast]);
 
   return (
-    <div className="col-span-2 grid justify-items-start gap-3 p-4">
-      {photo.visible && !isEmpty(photo.url) && (
-        <img
-          alt={name}
-          src={photo.url}
-          width={photo.filters.size}
-          height={photo.filters.size}
-          className={getPhotoClassNames(photo.filters)}
-        />
-      )}
-
-      <div>
-        <h1 className="mb-1">{name}</h1>
-        <p className="opacity-75">{headline}</p>
+    <>
+      <div className="col-span-2 grid justify-items-center pt-4">
+        {photo.visible && !isEmpty(photo.url) && (
+          <img
+            alt={name}
+            src={photo.url}
+            width={photo.filters.size}
+            height={photo.filters.size}
+            className={getPhotoClassNames(photo.filters)}
+          />
+        )}
       </div>
+      <div className="col-span-2 grid justify-items-start gap-3 p-4">
+        <div>
+          <h1 className="mb-1">{name}</h1>
+          <p className="opacity-75">{headline}</p>
+        </div>
 
-      <div className={clsx('flex flex-col gap-2.5', css(`svg { color: ${iconColor} }`))}>
-        <DataDisplay icon={<Room />} className="!gap-2 text-xs">
-          {formatLocation(location)}
-        </DataDisplay>
-
-        <DataDisplay icon={<Cake />} className="!gap-2 text-xs">
-          {formatDateString(birthdate, dateFormat)}
-        </DataDisplay>
-
-        <DataDisplay icon={<Email />} className="!gap-2 text-xs" link={`mailto:${email}`}>
-          {email}
-        </DataDisplay>
-
-        <DataDisplay icon={<Phone />} className="!gap-2 text-xs" link={`tel:${phone}`}>
-          {phone}
-        </DataDisplay>
-
-        <DataDisplay icon={<Public />} link={website && addHttp(website)} className="!gap-2 text-xs">
-          {website}
-        </DataDisplay>
-
-        {profiles.map(({ id, username, network, url }) => (
-          <DataDisplay key={id} icon={getProfileIcon(network)} link={url && addHttp(url)} className="!gap-2 text-xs">
-            {username}
+        <div className={clsx('flex flex-col gap-2.5', css(`svg { color: ${iconColor} }`))}>
+          <DataDisplay icon={<Room />} className="!gap-2 text-xs">
+            {formatLocation(location)}
           </DataDisplay>
-        ))}
+
+          <DataDisplay icon={<Cake />} className="!gap-2 text-xs">
+            {formatDateString(birthdate, dateFormat)}
+          </DataDisplay>
+
+          <DataDisplay icon={<Email />} className="!gap-2 text-xs" link={`mailto:${email}`}>
+            {email}
+          </DataDisplay>
+
+          <DataDisplay icon={<Phone />} className="!gap-2 text-xs" link={`tel:${phone}`}>
+            {phone}
+          </DataDisplay>
+
+          <DataDisplay icon={<Public />} link={website && addHttp(website)} className="!gap-2 text-xs">
+            {website}
+          </DataDisplay>
+
+          {profiles.map(({ id, username, network, url }) => (
+            <DataDisplay key={id} icon={getProfileIcon(network)} link={url && addHttp(url)} className="!gap-2 text-xs">
+              {username}
+            </DataDisplay>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
